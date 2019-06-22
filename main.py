932e7caf0b69e95e06eb1b8a59238e4953306801
Joris Gutjahr <joris.gutjahr@gmail.com>
2019-06-22 14:33:43 +0200

    main.py Executed on boot of the RPi Zero W or other RPi.

---
+++ main.py
@@ -0,0 +1,29 @@
+#! /usr/bin/env python3
+
+"""
+main.py
+This is executed on 
+boot of the RPi Zero W
+
+Functions:
+main(): Main function executing other functions
+"""
+
+"""
+main:
+Arguments:
+argv : Arguments on the Commandline
+Returns:
+Number, used to return to the OS.
+If you return 0, there is no Error,
+else there is.
+
+Comment:
+Execute this function and exit.
+"""
+def main(argv):
+    
+
+
+if __name__ == '__main__':
+    exit(main(sys.argv))
